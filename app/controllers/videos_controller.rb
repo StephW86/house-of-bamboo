a937d3241cd24b3a115c620959e7713f9b9303a2
Stephanie Wood <stephwood10@hotmail.com>
2025-05-03 15:14:38 +0100

    Add videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,5 @@
 class VideosController < ApplicationController
   def index
-    @video = view_context.asset_path("video_#{rand(1..4)}")
+    @video = view_context.asset_path("video_#{rand(1..6)}")
   end
 end
