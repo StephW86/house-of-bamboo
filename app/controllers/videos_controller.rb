a12d34bd9f352311da97ff59bf05f220076e9bac
Stephanie Wood <stephwood10@hotmail.com>
2025-05-27 19:40:06 +0100

    Plan for videos playing in order

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,13 @@
 class VideosController < ApplicationController
   def index
-    @video = view_context.asset_path("video_#{rand(1..6)}")
+    video_num = 1
+
+    if video_num < 7
+      video_num += 1
+      @video = view_context.asset_path("video_#{video_num}.mp4")
   end
 end
+
+# add next arrow when video ends
+# on clicking next video += 1
+# when the videos reach 6 go back to 1
