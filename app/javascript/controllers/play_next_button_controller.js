64a76a6f006a19b8b3317b61e9ffb9f44c4a3c41
Stephanie Wood <stephwood10@hotmail.com>
2025-06-12 16:08:13 +0100

    Remove console log

--- app/javascript/controllers/play_next_button_controller.js
+++ app/javascript/controllers/play_next_button_controller.js
@@ -4,10 +4,6 @@ export default class extends Controller {
   static values = { index: Number }
   static targets = ["video"]
 
-  connect() {
-    console.log("Controller connected");
-  }
-
   next() {
     this.indexValue = this.indexValue < 6 ? this.indexValue + 1 : 1;
     const nextSrc = `/assets/video_${this.indexValue}.mp4`;
