a54a6eb151431d12ad3b42a28839ed14d03d43fb
Stephanie Wood <stephwood10@hotmail.com>
2025-06-16 14:04:43 +0100

    Re-order videos. Try to render background

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,11 +1,9 @@
 import { Application } from "@hotwired/stimulus"
-import PlayNextButtonController from "./play_next_button_controller.js"
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
-application.register("play-next-button", PlayNextButtonController)
 
 export { application }
