75821753bb4a7200a328bc9d0aa25c3470e63c43
Stephanie Wood <stephwood10@hotmail.com>
2026-03-01 15:41:13 +0000

    change to single quotes

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -1,5 +1,5 @@
 class RedirectsController < ApplicationController
   def qrcode
-    redirect_to ENV.fetch("QRCODE_REDIRECT_URL", "/videos"), allow_other_host: false
+    redirect_to ENV.fetch('QRCODE_REDIRECT_URL', '/videos'), allow_other_host: false
   end
 end
